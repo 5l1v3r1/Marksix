8c2dde4951a95c28d711ff12751d4f1b7899cb03
HKLCF <hklcfs@gmail.com>
2014-03-17 15:27:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
-<?
-$title = 'Marksix v2.2';
+<?php
+$title = 'Marksix v2.3';
 $mode = 1; // 0 = Text mode; 1 = Image mode
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title><?=$title;?></title>
+<title><?php echo $title;?></title>
 </head>
 
 <body>
@@ -26,100 +26,49 @@ $mode = 1; // 0 = Text mode; 1 = Image mode
     </label>
   </form>
   
-<?
+<?php
 if($_GET['action'] == 'play') {
 	switch($_POST['colour']) {
 		case 'all':
-			$ball = array();
-			while(count($ball) < 6) {
-			$random_number = rand(1, 49);
-			$stop = false;
-				while(!$stop) {
-					if(array_search($random_number, $ball) === false) {
-						array_push($ball, $random_number);
-						$stop = true;
-					} else {
-						$random_number = rand(1, 49);
-					}
-				}
-			}
-			if($mode == 0) {
-				print join(', ', $ball);
-			} else {
-				foreach($ball as $value) {
-					echo "<img src=\"images/".$value.".gif\" alt=\"".$value."\" />";
-				}
-			}
+			$balls = 48;
+			$ball_number = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49);
+			$random_number = $ball_number[mt_rand(0, $balls)];
 			break;
 		case 'red':
-			$ball = array();
-			while(count($ball) < 6) {
+			$balls = 16;
 			$ball_number = array(1, 2, 7, 8, 12, 13, 18, 19, 23, 24, 29, 30, 34, 35, 40, 45, 46);
-			$random_number = $ball_number[rand(0, 16)];
-			$stop = false;
-				while(!$stop) {
-					if(array_search($random_number, $ball) === false) {
-						array_push($ball, $random_number);
-						$stop = true;
-					} else {
-						$random_number = $ball_number[rand(0, 16)];
-					}
-				}
-			}
-			if($mode == 0) {
-				print join(', ', $ball);
-			} else {
-				foreach($ball as $value) {
-					echo "<img src=\"images/".$value.".gif\" alt=\"".$value."\" />";
-				}
-			}
+			$random_number = $ball_number[mt_rand(0, $balls)];
 			break;
 		case 'green':
-			$ball = array();
-			while(count($ball) < 6) {
+			$balls = 15;
 			$ball_number = array(5, 6, 11, 16, 17, 21, 22, 27, 28, 32, 33, 38, 39, 43, 44, 49);
-			$random_number = $ball_number[rand(0, 15)];
-			$stop = false;
-				while(!$stop) {
-					if(array_search($random_number, $ball) === false) {
-						array_push($ball, $random_number);
-						$stop = true;
-					} else {
-						$random_number = $ball_number[rand(0, 15)];
-					}
-				}
-			}
-			if($mode == 0) {
-				print join(', ', $ball);
-			} else {
-				foreach($ball as $value) {
-					echo "<img src=\"images/".$value.".gif\" alt=\"".$value."\" />";
-				}
-			}
+			$random_number = $ball_number[mt_rand(0, $balls)];
 			break;
 		case 'blue':
-			$ball = array();
-			while(count($ball) < 6) {
+			$balls = 15;
 			$ball_number = array(3, 4, 9, 10, 14, 15, 20, 25, 26, 31, 36, 37, 41, 42, 47, 48);
-			$random_number = $ball_number[rand(0, 15)];
-			$stop = false;
-				while(!$stop) {
-					if(array_search($random_number, $ball) === false) {
-						array_push($ball, $random_number);
-						$stop = true;
-					} else {
-						$random_number = $ball_number[rand(0, 15)];
-					}
-				}
-			}
-			if($mode == 0) {
-				print join(', ', $ball);
-			} else {
-				foreach($ball as $value) {
-					echo "<img src=\"images/".$value.".gif\" alt=\"".$value."\" />";
-				}
-			}
+			$random_number = $ball_number[mt_rand(0, $balls)];
 			break;
+	}
+	$ball = array();
+	while(count($ball) < 6) {
+		$stop = false;
+		while(!$stop) {
+			if(array_search($random_number, $ball) === false) {
+				array_push($ball, $random_number);
+				$stop = true;
+			} else {
+				$random_number = $ball_number[mt_rand(0, $balls)];
+			}
+		}
+	}
+
+	if($mode == 0) {
+		print join(', ', $ball);
+	} else {
+		foreach($ball as $value) {
+			echo "<img src=\"images/".$value.".gif\" alt=\"".$value."\" />";
+		}
 	}
 }
 ?>
