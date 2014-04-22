8d0ab6273b79e66ccfbed908f1b89a9882779d6b
HKLCF <hklcfs@gmail.com>
2014-04-22 14:47:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$title = 'Marksix v2.3';
+$title = 'Marksix v2.4';
 $mode = 1; // 0 = Text mode; 1 = Image mode
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -22,6 +22,12 @@ $mode = 1; // 0 = Text mode; 1 = Image mode
       綠色
       <input type="radio" name="colour" id="blue" value="blue" />
       藍色
+      <input type="radio" name="colour" id="wored" value="wored" />
+      走紅色
+      <input type="radio" name="colour" id="wogreen" value="wogreen" />
+      走綠色
+      <input type="radio" name="colour" id="woblue" value="woblue" />
+      走藍色
       <input type="submit" name="submit" id="submit" value="攪珠" />
     </label>
   </form>
@@ -47,6 +53,21 @@ if($_GET['action'] == 'play') {
 		case 'blue':
 			$balls = 15;
 			$ball_number = array(3, 4, 9, 10, 14, 15, 20, 25, 26, 31, 36, 37, 41, 42, 47, 48);
+			$random_number = $ball_number[mt_rand(0, $balls)];
+			break;
+		case 'wored':
+			$balls = 31;
+			$ball_number = array(3, 4, 5, 6, 9, 10, 11, 14, 15, 16, 17, 20, 21, 22, 25, 26, 27, 28, 31, 32, 33, 36, 37, 38, 39, 41, 42, 43, 44, 47, 48, 49);
+			$random_number = $ball_number[mt_rand(0, $balls)];
+			break;
+		case 'wogreen':
+			$balls = 32;
+			$ball_number = array(1, 2, 3, 4, 7, 8, 9, 10, 12, 13, 14, 15, 18, 19, 20, 23, 24, 25, 26, 29, 30, 31, 34, 35, 36, 37, 40, 41, 42, 45, 46, 47, 48);
+			$random_number = $ball_number[mt_rand(0, $balls)];
+			break;
+		case 'woblue':
+			$balls = 32;
+			$ball_number = array(1, 2, 5, 6, 7, 8, 11, 12, 13, 16, 17, 18, 19, 21, 22, 23, 24, 27, 28, 29, 30, 32, 33, 34, 35, 38, 39, 40, 43, 44, 45, 46, 49);
 			$random_number = $ball_number[mt_rand(0, $balls)];
 			break;
 	}
