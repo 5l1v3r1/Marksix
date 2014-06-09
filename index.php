28d5eeba6c1aafb78a483aaa6d318b64c0cf347d
HKLCF <hklcfs@gmail.com>
2014-06-09 15:39:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $mode = 1; // 0 = Text mode; 1 = Image mode
 <html class="no-js">
 <head>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php echo $title;?></title>
 </head>
 
